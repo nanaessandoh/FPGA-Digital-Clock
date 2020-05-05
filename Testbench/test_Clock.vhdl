839b3fa984f59b2f3bb686584fdc7539dff2bfa1
Nana Essandoh <nanaessandoh@gmail.com>
2020-05-05 00:14:33 -0230

    Update test_Clock.vhdl

--- Testbench/test_Clock.vhdl
+++ Testbench/test_Clock.vhdl
@@ -29,18 +29,18 @@ end component;
 		
 	 test: digital_clock port map(	
 		 							SW 	=> SW_i,
-	 								KEY 	=> KEY_i,
+	 								KEY => KEY_i,
 									CLK	=> CLK_i,
-									LEDR	=> LEDR_i,
-									HEX5	=> HEX5_i,
-									HEX4	=> HEX4_i, 
-									HEX3	=> HEX3_i, 
-									HEX2	=> HEX2_i, 
-									HEX1	=> HEX1_i, 
-									HEX0	=> HEX0_i 
+									LEDR => LEDR_i,
+									HEX5 => HEX5_i,
+									HEX4 => HEX4_i, 
+									HEX3 => HEX3_i, 
+									HEX2 => HEX2_i, 
+									HEX1 => HEX1_i, 
+									HEX0 => HEX0_i 
 									);
 									
 	-- Generate Clock to Stack the System
-	 CLK_i <= not CLK_i after 10 ns;
+	 CLK_i <= not CLK_i after 0.5 ns;
 
     end behav;
